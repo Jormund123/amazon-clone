6e24af45fc2f25301ae1379ea7093b85cf19cd11
Jormund123 <anand.karna.social@gmail.com>
2022-09-14 16:28:11 +0530

    Login / Register Frontend

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import Header from "./Header/Header";
 import Home from "./Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout/Checkout";
+import Login from "./Login/Login";
 
 function App() {
     return (
         <BrowserRouter>
             {/* BEM */}
             <div className='App'>
-                <Header />
                 <Routes>
-                    <Route
-                        path='/checkout'
-                        element={
-                            <>
-                                <Checkout />
-                            </>
-                        }
-                    />
                     <Route
                         path='/'
                         element={
                             <>
+                                <Header />
                                 <Home />
                             </>
                         }
                     />
+                    <Route
+                        path='/checkout'
+                        element={
+                            <>
+                                <Header />
+                                <Checkout />
+                            </>
+                        }
+                    />
+                    <Route path='/login' element={<Login />} />
                 </Routes>
             </div>
         </BrowserRouter>
